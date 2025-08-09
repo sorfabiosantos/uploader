cbd60ff6110b65198a04394888f005a9dbb2ef99
Fábio Santos <fabiosantos@ifsul.edu.br>
2025-08-09 09:25:45 -0300

    Constantes do sistema

--- src/Uploader.php
+++ src/Uploader.php
@@ -5,7 +5,7 @@ namespace SorFabioSantos\Uploader;
  * Class Uploader
  * @author Fábio Santos <fabiosantos@ifsul.edu.br>
  * @package SorFabioSantos\Uploader
- * @version 1.0.4
+ * @version 1.0.5
  */
 class Uploader {
     /**
